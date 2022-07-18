3358efa6b04f0524c75c5a6c228f185b961de4d6
hamayungill <hamayunsafdar1947@gmail.com>
2022-07-18 13:58:14 +0500

    changes

--- src/components/template/template.js
+++ src/components/template/template.js
@@ -1,13 +1,9 @@
 import React, {useState, useEffect} from "react";
 import DragableInput from "../dragableInput/dragableInput";
 import '../template/template.css';
-import ImageUpload from 'image-upload-react';
-//important for getting nice style.
-import 'image-upload-react/dist/index.css';
 
-const Template = ({inputsData}) => {
+const Template = ({inputsData, selectElement, selectedElement, logoImg, bgImg}) => {
     const [inputsArr, setInputArr] = useState()
-    const [imageSrc, setImageSrc] = useState()
 
     useEffect(()=> {
         if(inputsData){
@@ -27,42 +23,50 @@ const Template = ({inputsData}) => {
         }
     }
 
-    const handleImageSelect = (e) => {
-      setImageSrc(URL.createObjectURL(e.target.files[0]))
+    const changeTxt = (inputId, value) => {
+        console.log(inputId, value)
+        const temp = inputsArr.map((e)=> {
+            if(e.id === inputId){
+                e.value = value
+            }
+            return e
+        })
+        console.log(temp)
+        setInputArr([...temp])
     }
 
     return (
         <div className="card-section">
             <h5>Design Your Card</h5>
-            
-            <div className="main-card">
-                
-                <div className="logo-img">
-                    <ImageUpload
-                            handleImageSelect={handleImageSelect}
-                            imageSrc={imageSrc}
-                            setImageSrc={setImageSrc}
-                            style={{
-                                width: 150,
-                                height: 150,
-                            }}
-                        />
-                </div>
+            <div className={`main-card backgroud-img ${selectedElement === "bg" ? "border-selected" : ""}`} style={{
+                backgroundImage: `url(${bgImg})`}} >
+                <div className="layer" onClick={()=> {selectElement("bg")}}></div>
+                {
+                    logoImg && 
+                    <div className={`logo-img ${selectedElement === "logo" ? "border-selected" : ""}`} onClick={()=> {selectElement("logo")}} >
+                            <img src={logoImg} />
+                    </div>
+                }
                 {
                     inputsArr?.map((e) => {
                         return (
-                            <div className="dragable-element" key = {e.id}>
-                                <DragableInput
-                                    top={e.top}
-                                    left={e.left}
-                                    width={e.width}
-                                    height={e.height}
-                                    rotateAngle={e.rotateAngle}
-                                    changeStyle={changeStyle}
-                                    inputId = {e.id}
-                                >
-                                    <div><p style={{fontSize: `${e.fontSize-3}px`}}>{e.value}</p></div>
-                                </DragableInput>
+                            <div className={`dragable-element ${selectedElement === e.id ? "active" : "inactive"}`} key = {e.id} onClick={()=> {selectElement(e.id)}}>
+                                <div className="input-div">
+                                    {/* {
+                                        selectedElement === e.id ? <input type="text" name={e.name} defaultValue={e.value} onChange={(x)=>{changeTxt(e.id, x.target.value)}} />: ""
+                                    } */}
+                                    <DragableInput
+                                        top={e.top}
+                                        left={e.left}
+                                        width={e.width}
+                                        height={e.height}
+                                        rotateAngle={e.rotateAngle}
+                                        changeStyle={changeStyle}
+                                        inputId = {e.id}
+                                    >
+                                        <div><p style={{fontSize: `${e.fontSize-3}px`}}>{e.value}</p></div>
+                                    </DragableInput>
+                                </div>
                             </div>
                         )
                     })
--- src/pages/editor.js
+++ src/pages/editor.js
@@ -2,17 +2,49 @@ import React, { useState, useEffect } from "react";
 import ManageInput from "../components/manageInputs/manageInputs";
 import Template from "../components/template/template";
 
+const defaultBg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEEuSKv1VSK9XoW7PdK-Lpm0TmcW6RfFL9Xw&usqp=CAU"
+const defaultLogo = "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80"
+
 const Editor = () => {
     const [inputsData, setInputsData] = useState()
-    
+    const [selectedElement, setSelectedElement] = useState() //logo, bg
+    const [logoImg, setLogoImg] = useState(defaultLogo)
+    const [bgImg, setBgImg] = useState(defaultBg)
+
+    const replaceImg = (e) => {
+        const imgUrl = URL.createObjectURL(e.target.files[0])
+        if(selectedElement === "logo"){
+            setLogoImg(imgUrl)
+        }else if(selectedElement === "bg"){
+            setBgImg(imgUrl)
+        }
+    } 
+
+    const removeImg = () => {
+        if(selectedElement === "logo"){
+            setLogoImg("")
+        }else if(selectedElement === "bg"){
+            setBgImg("")
+        }
+    }
+
     return (
         <div className="main-editor">
             <div className="row">
-                <div className="col-4">
+                <div className="col-md-4">
                     <ManageInput setData={setInputsData}/>
+                    <div className="btn-img">
+                        <input type="file" className="imgup" id="imgupload" onChange={(e)=> {replaceImg(e)}} />
+                        <button>
+                        <label htmlFor='imgupload'><a id="OpenImgUpload">Replace Image</a></label>
+                        </button>
+                        <button onClick={() => {removeImg()}}>Remove Image</button>
+                    </div>
                 </div>
-                <div className="col-8">
-                    <Template inputsData={inputsData}/>
+                <div className="col-md-8">
+                    <div>
+                        <Template inputsData={inputsData} selectElement={setSelectedElement} selectedElement={selectedElement} logoImg= {logoImg} bgImg={bgImg}/>  
+                    </div>
                 </div>
             </div>
         </div>
